a18c6c285e1d2d4836a08f62c112cd69f6e824fc
David Josephs <josephsd@smu.edu>
2019-01-13 18:20:29 -0600

    fixed install script

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -9,11 +9,11 @@ source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.vim_runtime/vimrcs/init.vim
+source ~/.vim_runtime/vimrcs/rrc.vim
 
 try
 source ~/.vim_runtime/my_configs.vim
-source ~/.vim_runtime/vimrcs/init.vim
-source ~/.vim_runtime/vimrcs/rrc.vim
 catch
 endtry' > ~/.vimrc
 
